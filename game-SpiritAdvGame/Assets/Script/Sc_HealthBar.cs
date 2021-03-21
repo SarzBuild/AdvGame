df878adba1da47ecca738396dfeaf23c121bee76
Myrandagau <73671431+Myrandagau@users.noreply.github.com>
2021-03-21 02:12:25 -0400

    Ghost sprite + HealthBar + UI

---
+++ game-SpiritAdvGame/Assets/Script/Sc_HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sc_HealthBar : MonoBehaviour
+{
+    public float currentHP;
+    public int maxHP = 200;
+    public Image sliderHP;
+
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentHP -= 5;
+        }
+        UpdateSlider();
+    }
+
+    void UpdateSlider()
+    {
+        sliderHP.fillAmount = currentHP / maxHP;
+    }
+}
