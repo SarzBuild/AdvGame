680b1877623972f78ce055ecc2f2dad0d8cb6faa
Gabriel Bulcao Rachman <gabrielrachman@hotmail.com>
2021-03-27 12:14:24 -0400

    Handle attack and upgrades

--- game-SpiritAdvGame/Assets/Script/Sc_PlayerControler.cs
+++ game-SpiritAdvGame/Assets/Script/Sc_PlayerControler.cs
@@ -8,9 +8,12 @@ public class Sc_PlayerControler : MonoBehaviour
 
     float horizontal;
     float vertical;
+    string directionFacing = "down";
     float moveLimiter = 0.7f;
 
-    public float runSpeed = 4f;
+    public float runSpeed = 4.0f;
+
+    bool isAttacking = false;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -20,8 +23,29 @@ public class Sc_PlayerControler : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
         vertical = Input.GetAxisRaw("Vertical"); // -1 is down
-    }
 
+        ManageDirectionFaced();
+        Attack();
+    }
+    void ManageDirectionFaced()
+    {
+        if (horizontal < 0 )
+        {
+            directionFacing = "Left";
+        }
+        else if (horizontal > 0)
+        {
+            directionFacing = "Right";
+        }
+        else if (vertical > 0)
+        {
+            directionFacing = "Up";
+        }
+        else if (vertical < 0)
+        {
+            directionFacing = "Down";
+        }
+    }
     private void FixedUpdate()
     {
         if (horizontal != 0 && vertical != 0) 
@@ -31,5 +55,21 @@ public class Sc_PlayerControler : MonoBehaviour
         }
 
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isAttacking)
+        {
+            isAttacking = true;
+            StartCoroutine(DoAttack()); 
+        }
+    }
+    IEnumerator DoAttack()
+    {
+        transform.Find("Hitbox" + directionFacing).gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        transform.Find("Hitbox" + directionFacing).gameObject.SetActive(false);
+        isAttacking = false;
     }
 }
---
+++ game-SpiritAdvGame/Assets/Script/Sc_SelectChildObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_SelectChildObject : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Legs")
+        {
+            mySpriteRenderer.enabled = false;
+            Destroy(collision.gameObject);
+            transform.Find("PlayerC").gameObject.SetActive(false);
+            transform.Find("PlayerB").gameObject.SetActive(false);
+            transform.Find("PlayerA").gameObject.SetActive(true);
+            
+        }
+        if (collision.name == "Body" && transform.Find("PlayerA").gameObject.activeSelf)
+        {
+            transform.Find("PlayerA").gameObject.SetActive(false);
+            transform.Find("PlayerC").gameObject.SetActive(false);
+            transform.Find("PlayerB").gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+        if (collision.name == "Helmet" && transform.Find("PlayerB").gameObject.activeSelf)
+        {
+            transform.Find("PlayerA").gameObject.SetActive(false);
+            transform.Find("PlayerB").gameObject.SetActive(false);
+            transform.Find("PlayerC").gameObject.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
