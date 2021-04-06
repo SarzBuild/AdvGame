497d3df0acf37834a5f890ee329471f0b2b940cd
SarzBuild <jrmietn@gmail.com>
2021-04-05 23:18:23 -0400

    Updated Camera System and Tilemap Rendering
    Camera System now works with two different colliders, one for the bounding box of the camera, the other one for room changer.
    Tilemap rendering is done depending on the y axis of the player.

---
+++ game-SpiritAdvGame/Assets/Script/Puzzles/Tutorial Puzzle/Sc_Puzzle_TutorialButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_Puzzle_TutorialButton : MonoBehaviour
+{
+    public static bool openDoor;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            openDoor = true;
+        }
+    }
+}
