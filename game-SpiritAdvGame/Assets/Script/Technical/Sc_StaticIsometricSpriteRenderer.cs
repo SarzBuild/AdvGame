f3a200b3111a1b9398b991993d69c54b0c1a4b65
SarzBuild <jrmietn@gmail.com>
2021-04-05 23:20:17 -0400

    Revert "Revert "Updated Camera System and Tilemap Rendering""
    This reverts commit beb8773b7b5db611b28fff2615dc09d451a9a006.

---
+++ game-SpiritAdvGame/Assets/Script/Technical/Sc_StaticIsometricSpriteRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[ExecuteInEditMode]
+public class Sc_StaticIsometricSpriteRenderer : MonoBehaviour
+{
+    public GameObject player;
+    void Awake()
+    {
+        GetComponent<TilemapRenderer>().sortingOrder = (int) (transform.parent.position.y * -10);
+        UpdateRenderOrder();
+    }
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void UpdateRenderOrder()
+    {
+        if (player.GetComponent<SpriteRenderer>().sortingOrder < gameObject.GetComponent<TilemapRenderer>().sortingOrder)
+        {
+            gameObject.GetComponent<TilemapRenderer>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<TilemapRenderer>().enabled = true;
+        }
+    }
+}
