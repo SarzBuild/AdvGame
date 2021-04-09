de3a631b8d39b9be07a329eb91063b76b76457aa
SarzBuild <jrmietn@gmail.com>
2021-04-09 18:52:32 -0400

    some lil changes

---
+++ game-SpiritAdvGame/Assets/Sc_BoundingBoxLvlTwo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_BoundingBoxLvlTwo : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        while (!Sc_UpperLvlColliderListener.isStillOnTop)
+        {
+            if (other.tag == "Enemy")
+            {
+                
+                
+            }
+        }
+    }
+}
--- game-SpiritAdvGame/Assets/Script/Technical/Sc_UpperLvlColliderListener.cs
+++ game-SpiritAdvGame/Assets/Script/Technical/Sc_UpperLvlColliderListener.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class Sc_UpperLvlColliderListener : MonoBehaviour
 {
     public static bool hasJumpedDown;
+    public static bool isStillOnTop;
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
             hasJumpedDown = false;
+            isStillOnTop = true;
         }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Enemy")
-        {
-            hasJumpedDown = true;
-        }
-        
     }
 }
